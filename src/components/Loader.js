d53551b98699e0b8c9a316d87eb1e2084e573290
EC2 Default User <ec2-user@ip-172-31-90-228.ec2.internal>
2020-11-13 18:14:43 +0000

    add spinner

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Spinner from 'react-bootstrap/Spinner'
+
 
 class Loader extends Component {
   render() {
     return(
         <div>
-        <h1>Loading</h1>
+        <h1>Loading <Spinner animation="grow" variant="primary" /><Spinner animation="grow" variant="primary" /><Spinner animation="grow" variant="primary" /></h1>
+ 
         </div>
         )
     ;
